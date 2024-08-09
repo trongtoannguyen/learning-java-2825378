7b09bdb73e713af009ed59f0f8c5bd5fd0378135
Nguyen Trong Toan <trongtoan1609ht@gmail.com>
2024-08-09 10:06:50 +0000

    complete 02 chapter challenge

---
+++ src/Answer.java
@@ -0,0 +1,8 @@
+class Answer {
+    // Find the last character in the string (text)
+    static char findLastCharacter(String text) {
+        // Your code goes here.
+        char lastCharacter = text.charAt(text.length() - 1);
+        return lastCharacter;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
-        System.out.println(studentFirstName + " " +
-                studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update it to?");
-
         Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
-
-        System.out.println(studentFirstName + " " + studentLastName +
-                " now has a GPA of " + studentGPA);
+        System.out.println("Let's take an input...");
+        String text = input.nextLine();
+        char result = Answer.findLastCharacter(text);
+        System.out.println("Result: " + result);
+        input.close();
     }
 }
