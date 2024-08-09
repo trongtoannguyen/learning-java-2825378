eb00f33b4d4848c4c854442493de6f99d12404a0
Nguyen Trong Toan <trongtoan1609ht@gmail.com>
2024-08-09 18:51:25 +0000

    complete 03_challenge leap year tracking

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,25 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String args[]) {
-        String question = "What is the largest planet in our solar system?";
-        String choiceOne = "earth";
-        String choiceTwo = "jupiter";
-        String choiceThree = "saturn";
+        int year = 2020;
 
-        String correctAnswer = choiceTwo;
-
-        // Write a print statement asking the question
-        System.out.println(question);
-
-        // Write a print statement giving the answer choices
-        System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
-
-        // Have the user input an answer
-        Scanner scanner = new Scanner(System.in);
-        // Retrieve the user's input
-        String input = scanner.next();
-
-        // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
-        if(correctAnswer.equals(input.toLowerCase())) {
-            System.out.println("Congrats! That's the correct answer");
-        } else {
-            System.out.println("You are incorrect. The correct answer is " + correctAnswer);
+        boolean result = Answer.isLeapYear(year);
+        if (result) {
+            System.out.println(year + " is leap year");
+        }else{
+            System.out.println(year + " is no leap year");
         }
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+    }
 
+}
+
+class Answer {
+
+    static boolean isLeapYear(int year) {
+        // Your code goes here.
+        if(year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))
+            return true;
+        return false;
     }
 
 }
