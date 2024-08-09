4ff0c203fdf54ceeda94d30149ad565e76d25dc4
Nguyen Trong Toan <trongtoan1609ht@gmail.com>
2024-08-09 09:45:04 +0000

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        int age = 21;
+        String name = "Toan Nguyen";
+        boolean legal = age >= 18;
+
+        System.out.println("Hello " + name + " ,you're " + age + " years old");
         
+        String msg = legal ? ("You're legal to drink") : ("You're not allowed to drink this beer");
+        System.out.println(msg);
     }
 }
