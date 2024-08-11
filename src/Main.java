e80ede5b097cab0a2ea5bd16149b67235da10341
Nguyen Trong Toan <trongtoan1609ht@gmail.com>
2024-08-11 17:57:40 +0000

    complete 05_challenge move capitalism to the front of the line

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
+        String s = "Hello World";
+        String result = Answer.moveCapitalLettersToFront(s);
+        System.out.println(s + "==>" + result);
+    }
+}
 
-        Math.
+class Answer {
+
+    static String moveCapitalLettersToFront(String s) {
+        // Your code goes here.
+        StringBuffer result = new StringBuffer();
+        int capitalIndex = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result.insert(Math.min(capitalIndex++, s.length()), ch); //increase capitalIndex value by 1 after invoke
+            }else{
+                result.append(ch);
+            }
+
+        }
+        return result.toString();
     }
 
 }
